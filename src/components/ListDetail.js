3516416c70e64e27fd8c406d5c331bdf03b7a3f4
Joanna Huang <joannathhuang@gmail.com>
2020-08-29 09:09:53 -0400

    [Feat]: updating css for delete

--- src/components/ListDetail.js
+++ src/components/ListDetail.js
@@ -2,14 +2,15 @@ import React from "react";
 
 const ListDetail = ({ item, deleteItem }) => {
   return (
-    <li className="list-details">
-      <div>
-        <h5 className="card-title">{item.title}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{item.year}</h6>
-      </div>
-      <div>
-        <button onClick={() => deleteItem(item.id)}>X</button>
-      </div>
+    <li className="list-details" onClick={() => deleteItem(item.id)}>
+      {/* <div> */}
+      <h5 className="card-title">{item.title}</h5>
+      <h6 className="card-subtitle mb-2 text-muted">{item.year}</h6>
+      {/* </div> */}
+
+      {/* <div>
+        // <button onClick={() => deleteItem(item.id)}>X</button>
+      </div> */}
     </li>
     //   <li className="list-group-item">
     //   <h5 className="card-title">{item.title}</h5>
