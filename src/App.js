712ea2d5c43172736e4e8abc432747b51284e736
Joanna Huang <joannathhuang@gmail.com>
2020-08-29 09:46:23 -0400

    [Feat]: added banner and updated css

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import SearchBar from "./components/SearchBar";
 import MovieList from "./components/MovieList";
 import NomList from "./components/NomList";
 import SearchContextProvider from "./contexts/SearchContext";
+import Banner from "./components/Banner";
 
 const App = () => {
   return (
     <div className="App">
       <SearchContextProvider>
-        {/* <div className="search-container"> */}
-        <SearchBar />
-        <MovieList />
-        {/* </div> */}
+        <Banner />
+        <div className="search-container">
+          <SearchBar />
+          <MovieList />
+        </div>
         <NomList />
       </SearchContextProvider>
     </div>
