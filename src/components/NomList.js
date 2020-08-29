b63bba863a9da7f406c42d6e04edc663e41ca8c4
Joanna Huang <joannathhuang@gmail.com>
2020-08-29 09:53:45 -0400

    [Feat]: added and experimenting with react-smooth-dnd

--- src/components/NomList.js
+++ src/components/NomList.js
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import ListDetail from "./ListDetail";
 import { SearchContext } from "../contexts/SearchContext";
+import { Container, Draggable } from "react-smooth-dnd";
 
 const NomList = () => {
   const { list, deleteItem } = useContext(SearchContext);
   return (
     <div className="nom-container">
       <h3>Nominees</h3>
-      <ul className="list-container">
+      {/* <ul className="list-container">
         {list.length >= 1 ? (
           list.map((item) => (
             <ListDetail item={item} key={item.id} deleteItem={deleteItem} />
@@ -15,7 +16,18 @@ const NomList = () => {
         ) : (
           <em>You know nothing, Jo Snow</em>
         )}
-      </ul>
+      </ul> */}
+      <div>
+        <Container>
+          {list.map((item) => {
+            return (
+              <Draggable>
+                <ListDetail item={item} key={item.id} deleteItem={deleteItem} />
+              </Draggable>
+            );
+          })}
+        </Container>
+      </div>
     </div>
   );
 };
