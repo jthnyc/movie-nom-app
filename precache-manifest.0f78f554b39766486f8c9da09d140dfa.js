5976dd7d8edcb28a80ced641ebdf498a4304d58a
Joanna Huang <joannathhuang@gmail.com>
2020-09-04 17:39:31 -0400

    Updates

---
+++ precache-manifest.0f78f554b39766486f8c9da09d140dfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156b302af0df078a1cf0f2c3a7961691",
+    "url": "/movie-nom-app/index.html"
+  },
+  {
+    "revision": "509366a13bb7d823ec7d",
+    "url": "/movie-nom-app/static/css/main.ce0a99db.chunk.css"
+  },
+  {
+    "revision": "250aed70cd2a9799658f",
+    "url": "/movie-nom-app/static/js/2.3f50b929.chunk.js"
+  },
+  {
+    "revision": "4f1074383207016e7b8212bac46ab5ee",
+    "url": "/movie-nom-app/static/js/2.3f50b929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509366a13bb7d823ec7d",
+    "url": "/movie-nom-app/static/js/main.8553a051.chunk.js"
+  },
+  {
+    "revision": "96851a29afead465b416",
+    "url": "/movie-nom-app/static/js/runtime-main.ce40c716.js"
+  }
+]);
