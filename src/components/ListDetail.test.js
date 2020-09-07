fce92f1a0bd9ab0358035c49808837578d07fe7e
Joanna Huang <joannathhuang@gmail.com>
2020-09-07 15:38:38 -0400

    [Feat]: updated CSS and ReadMe

--- src/components/ListDetail.test.js
+++ src/components/ListDetail.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import ListDetail from "./ListDetail";
 
 describe("ListDetail Component", () => {
-  test("renders list items", () => {
+  test("renders list items with starIcon", () => {
     render(<ListDetail />);
     expect(screen.getByAltText("starIcon")).toBeInTheDocument();
   });
--- src/components/MovieDetail.test.js
+++ src/components/MovieDetail.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import MovieDetail from "./MovieDetail";
 
 describe("MovieDetail Component", () => {
-  test("renders movie items", () => {
+  test("renders movie items with film icon", () => {
     render(<MovieDetail />);
     expect(screen.getByAltText("filmIcon")).toBeInTheDocument();
   });
